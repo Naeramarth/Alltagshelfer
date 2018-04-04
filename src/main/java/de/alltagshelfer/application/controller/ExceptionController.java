858a367e3422679afa39f966885a1924be3df38a
Naeramarth <Blood.Work@gmx.at>
2018-04-04 13:27:14 +0200

    imports ausgebessert

--- src/main/java/de/alltagshelfer/application/controller/ExceptionController.java
+++ src/main/java/de/alltagshelfer/application/controller/ExceptionController.java
@@ -1,13 +1,9 @@
 package de.alltagshelfer.application.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
